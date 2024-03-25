7055004cf277c63ba04653e1fb319512747ef35b
Firtnn <89849261+Firtnn@users.noreply.github.com>
2024-03-25 09:30:50 +0100

    Update FixedFollowView.cs

--- Assets/Script/FixedFollowView.cs
+++ Assets/Script/FixedFollowView.cs
@@ -17,12 +17,29 @@ public class FixedFollowView : AView
     [SerializeField] private float yawOffsetMax;
     [SerializeField] private float ptichOffsetMax;
     
+    
     void Start()
     {
         Vector3 dir = target - transform.position;
+        Vector3 centaleDir = centralPoint.transform.position - transform.position;
+        
         camConfig.yaw = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
         camConfig.pitch = (Mathf.Asin(dir.y) * -1) * Mathf.Rad2Deg;
     }
-    
+
+
+    public override CameraConfiguration GetConfiguration()
+    {
+        Vector3 dir = target - transform.position;
+        Vector3 centraleDir = centralPoint.transform.position - transform.position;
+        float yawOffset = Vector3.SignedAngle(centraleDir, dir, Vector3.up);
+        yawOffset = Mathf.Clamp(yawOffset, -yawOffsetMax, yawOffsetMax);
+        
+        float centralYaw = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
+        camConfig.yaw = centralYaw + yawOffset;
+        return camConfig;
+    }
+
+   
     
 }
